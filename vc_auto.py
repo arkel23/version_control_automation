d97e68092893f9bae27acb3853dfcca376ad3725
ed_laptop_asus <edwinarkelrios.eic08g@nctu.edu.tw>
2020-04-09 02:04:52 +0800

    Automatic commit done at: 2020-04-09_02-04-39

--- vc_auto.py
+++ vc_auto.py
@@ -1,51 +1,217 @@
-import argparse, os, sys, path, platform
+import argparse, os, sys, path, platform, datetime
 import subprocess
 from subprocess import Popen, PIPE
-from datetime import date
 
-def day_start():
-    pass
-    # first go into each tracked repo then pull from rem master into local master
-    # log into vc_auto_log.txt
+def add_folders(repo_path_tracker):
+    #prompt user for adding paths
 
-def day_end():
+    while True:
+        inp = input('Please copy full path to folder. Leave empty to continue: ')
+        if inp=='':
+            break
+        else:
+            if (os.path.exists(inp)):
+                repo_path_tracker.write(os.path.abspath(inp))
+            else:
+                print('Directory not valid. Please add a correct directory.')
+
+def verify_folders():
+    #checks each folder in a repo_path_tracker to verify it is local repo
+    #and is setup to push and pull to remote repo
+    repo_path_tracker = open('repo_path_tracker.txt', 'r')
+    r_c = 0
+
+    for curline in repo_path_tracker:
+        if not curline.startswith('#'):
+            git_exist = os.path.isdir(os.path.join(curline,'.git'))
+            if (git_exist == False):
+                r_c = 1
+                break
+    
+    repo_path_tracker.close()
+    return r_c
+
+
+def run_subprocess(cmd, stdout=False):
+    if (stdout == True):
+        p = Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE,)
+        stdout, stderr = p.communicate()
+        return p.returncode, stdout
+    else:
+        p = Popen(cmd)
+        stdout, stderr = p.communicate()
+        return p.returncode
+
+def push_subop(branch, curr_time, master=False):
+    
+    if (master==True):
+        branch = 'branch_{}'.format(curr_time)
+        cmd = 'git checkout -b {}'.format(branch)
+        r_c = run_subprocess(cmd)
+        if (r_c != 0):
+            print('Problem with dir {}, branch: {} in checkout')
+            return r_c
+    
+    cmd = 'git checkout {}'.format(branch) #change to local_branch_n
+    r_c = run_subprocess(cmd)
+    if (r_c != 0):
+        print('Problem with dir {}, branch: {} in checkout')
+        return r_c
+    cmd = 'git add .'
+    r_c = run_subprocess(cmd)
+    if (r_c != 0):
+        print('Problem with dir {}, branch: {} in add')
+        return r_c
+    cmd = 'git commit -m "Automatic commit done at: {}"'.format(curr_time)
+    r_c = run_subprocess(cmd)
+    if (r_c != 0):
+        print('Problem with dir {}, branch: {} in commit')
+        return r_c
+
+    # push remotely
+    cmd = 'git push origin {}'.format(branch)
+    r_c = run_subprocess(cmd)
+    if (r_c != 0):
+        print('Problem with dir {}, branch: {} in push')
+        return r_c
+    
+    return r_c
+
+def pull_push(curline, log_vc_auto, curr_time):
+    os.chdir(curline)
+
+    # pull operation
+    
+    cmd = 'git pull origin master'
+    r_c = run_subprocess(cmd)
+    log_msg = 'Pull operation for {} finished with return code : {}'.format(
+            curline, r_c) 
+    log_vc_auto.write(curr_time + '\t' + log_msg)
+    
+    # push operation
+    cmd = 'git branch -a'
+    r_c, out = run_subprocess(cmd, stdout=True)
+    #print(out) #prints "b '{local_branch_1}\n{local_branch_2}\n{remote_branch_n}\n" in byte
+    encoding = 'utf-8'
+    decoded_out = str(out[:-1], encoding)
+    out_clean = decoded_out.replace(' ', '').replace('*', '')
+    branch_list = out_clean.split('\n')
+    branch_list = [branch.replace('\n', '') for branch in branch_list]
+    branch_list = [branch for branch in branch_list if 'remote' not in branch]
+
+    for branch in branch_list:
+        if branch != 'master':
+            r_c = push_subop(branch, curr_time)
+            log_msg = 'Push operation for: {}, branch: {} finished with return code : {}'.format(
+                curline, branch, r_c) 
+            log_vc_auto.write(curr_time + '\t' + log_msg)
+        else:
+            r_c = push_subop(branch, curr_time, master=True)
+            log_msg = 'Push operation for: {}, branch: {} finished with return code : {}'.format(
+                curline, branch, r_c) 
+            log_vc_auto.write(curr_time + '\t' + log_msg)
+            
+
+def task_scheduler_mac(path_exe, path_curr, time_run1, time_run2):
+    # makes a task scheduler using cron which runs on fixed times
+    # to do cron
     pass
-    #at certain time stage first check which branch it's being worked on
-    #if not master then stage, commit and push remotely. Log into vc_auto_log.txt
-    #if master then create temp_branch, stage, commit and push remotely. Log
+
+def task_scheduler_linux(path_exe, path_curr, time_run1, time_run2):
+    # makes a task scheduler using cron which runs on fixed times
+    # to do cron
+    pass
+
+def task_scheduler_win(path_exe, path_curr, time_run1, time_run2):
+    # makes a task schedule script which runs on fixed times
+
+    #syntax: /CREATE /SC [MINUTE/HOURLY/DAILY/WEEKLY/MONTHLY/
+    # ONCE/ONSTART/ONLOGON/ONIDLE/ONEVENT] /D [MON to SUN or 1-31 or *]
+    # /TN [TASK NAME AND LOCATION] /TR [LOCATION AND NAME OF TASK TO RUN]
+    # /ST [TIME TO TRUN TASK (24 HOURS FORMAT)]
+
+    bat_name = 'task_scheduler.bat'
+    task_scheduler = open(bat_name, 'w')
+    task_scheduler.write('''SCHTASKS /CREATE /SC DAILY /TN "vc_auto_1" /TR "{} {}" /ST {}:00\n'''.format(path_exe, path_curr, time_run1))
+    task_scheduler.write('''SCHTASKS /CREATE /SC DAILY /TN "vc_auto_2" /TR "{} {}" /ST {}:00'''.format(path_exe, path_curr, time_run2))
+    task_scheduler.close()
+
+    r_c = run_subprocess(bat_name)
+    
+    return r_c
+
+def initialize(curr_time, curr_os):
+    log_vc_auto = open('log_vc_auto.txt', 'w')
+    log_vc_auto.write('curr_time\tlog_msg\n')
+
+    repo_path_tracker = open('repo_path_tracker.txt', 'w')
+    repo_path_tracker.write('#local_repo_path\n')
+
+    time_run1 = input('Fixed time 1 to run script(number from 13-23): ')
+    time_run2 = input('Fixed time 2 to run script(number from 1-23): ')
+
+    try:
+        exe = sys.executable #python executabe path
+        path_exe = path.Path(exe)  #path for the exe
+        path_curr = path.Path(os.getcwd())
+    except:
+        log_msg = 'Error in either executable or current path. Closing prematurely.'
+        log_vc_auto.write(curr_time + '\t' + log_msg)
+        log_vc_auto.close()
+        repo_path_tracker.close()
+
+    print('OS:', curr_os)
+    if (curr_os == 'Linux'):
+        task_scheduler_linux(path_exe, path_curr, time_run1, time_run2)
+        #to do: log
+    elif (curr_os == 'Darwin'):
+        task_scheduler_mac(path_exe, path_curr, time_run1, time_run2)
+        #to do: log 
+    elif (curr_os == 'Windows'):
+        rc = task_scheduler_win(path_exe, path_curr, time_run1, time_run2)
+        log_msg = 'Return code for initializing task_scheduler_win: {}\n'.format(rc)
+        log_vc_auto.write(curr_time + '\t' + log_msg)
+    else:
+        log_msg = 'Invalid OS: {}'.format(curr_os)
+        log_vc_auto.write(curr_time + '\t' + log_msg + '\n')
+
+    add_folders(repo_path_tracker)
+    repo_path_tracker.close()
+        
+    return log_vc_auto
 
 def check_setup(ft):
-    #check if first time setup or update
     print('First time setup: ', ft)
-    # check platform and adjust steps based on system:
-    # setup cron or windows scheduler/mac to run the script at certain times
-    # only needed for the first time. script should save this info so it is automated next
-    # save or create a new .bash or something file
+    curr_time = str(datetime.datetime.now()).replace(' ','_').replace(':','-')
+    curr_time = curr_time.split('.', 1)[0]
     curr_os = platform.system()
-    if (curr_os == 'Linux'):
-        print('OS:', curr_os)
-    elif (curr_os == 'Darwin'):
-        print('OS:', curr_os)
-    elif (curr_os == 'Windows'):
-        print('OS:', curr_os)
+    
+    #initializes log, path tracker and scheduler if first time, else runs updating
+    if ft:
+        log_vc_auto = initialize(curr_time, curr_os) 
     else:
-        print('OS:', curr_os)
-        #log irregular OS into vc_auto_log.txt
-        sys.exit(1)
+        log_vc_auto = open('log_vc_auto.txt', 'a')
+    
+    r_c = verify_folders()
+    log_msg = 'Folder verification return code: {}\n'.format(r_c)
+    log_vc_auto.write(curr_time + '\t' + log_msg)
+    
+    # to do: go through each folder in repo_path_tracker.txt, pull then push
+    repo_path_tracker = open('repo_path_tracker.txt', 'r')
+    
+    for curline in repo_path_tracker:
+        if not curline.startswith('#'):
+            pull_push(curline, log_vc_auto, curr_time)
 
-
-    #check credentials (git config user.name and user.email)
-    #check each folder to contain .git folder
-    # if everything normal then return 0, otherwise 1 and shutdown
-
+    repo_path_tracker.close()
+    log_vc_auto.close()
+    
 def main():
     """
-    to do: 
-    read input parameters (initialize new path tracker file, update)
     check integrity of path repos and os
-    startup routine (day_start)
-    end routine (day_end)
-    
+    linux and mac scheduling routine (2 fixed times)
+    each fixed time: pull from remote master into local master, 
+    then push changes from local branches (no master) to remote
     """
     parser = argparse.ArgumentParser()
     parser.add_argument('-ft', required=False, 
